13a262edc66f0f2ef3968c54361800f6c1da17a9
dingjun <dingjun168@gmail.com>
2018-07-29 14:25:58 -0700

    Add slider to split screen

--- Assets/Scripts/UI/SplitScreen.cs
+++ Assets/Scripts/UI/SplitScreen.cs
@@ -12,6 +12,7 @@ namespace InventorySystem
 		private int _splitCount;
 
 		public InputField StackCountInputField;
+		public Slider StackCountSlider;
 
 		public int SplitCount
 		{
@@ -24,6 +25,7 @@ namespace InventorySystem
 				int validValue = Mathf.Clamp(value, 1, _stackable.Count);
 				_splitCount = validValue;
 				StackCountInputField.text = _splitCount.ToString();
+				StackCountSlider.value = _splitCount;
 			}
 		}
 
@@ -32,13 +34,23 @@ namespace InventorySystem
 			Debug.Assert(itemIcon.Item is IStackable);
 			_itemIcon = itemIcon;
 			_stackable = (IStackable)_itemIcon.Item;
+
+			StackCountSlider.minValue = 1;
+			StackCountSlider.maxValue = _stackable.Count;
+
 			SplitCount = _stackable.Count;
+			
 			gameObject.SetActive(true);
 		}
 		
-		public void OnEndEdit()
+		public void OnEndEditInputField()
 		{
 			SplitCount = int.Parse(StackCountInputField.text);
+		}
+
+		public void OnValueChangedSlider()
+		{
+			SplitCount = (int)StackCountSlider.value;
 		}
 
 		public void OnStep(int step)
